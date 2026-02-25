f7d2aa99e973f42bc6b049ecf0e8d3806ace9a7a
j-preim <j.preimesberger@gmail.com>
2026-02-25 11:54:58 -0600

    test draft json

--- api/index.js
+++ api/index.js
@@ -22,9 +22,9 @@ app.post("/test-cors-post", function (req, res) {
   res.status(200).json({ success: true });
 });
 
-// send the about.html file
-app.get("/about", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "static", "about.html"));
+// send the draft JSON file
+app.get("/keepers", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "static", "updated_draft_recap_2009.json"));
 });
 
 app.listen(4000, () => console.log("Server ready on port 4000."));
